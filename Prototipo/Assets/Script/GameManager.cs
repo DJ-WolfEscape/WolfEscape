7e209bef4b9cfce607b096c3c6aa984a9b77f420
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2021-12-29 16:14:05 +0000

    Score adicionado e Game Manager fix
    Fix GameManager

--- Prototipo/Assets/Script/GameManager.cs
+++ Prototipo/Assets/Script/GameManager.cs
@@ -1,22 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MenuScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    //menu pausa
     public GameObject menu;
     public GameObject menuAdio;
     public static bool GameIsPaused;
 
+    //counter score
+    private GameObject player;
+    public Text uiDistance;
+
+    public GameObject Player { get => player; set => player = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Player = GameObject.Find("Player");   
     }
 
     // Update is called once per frame
     void Update()
     {
+        //counter score
+        int distance = Mathf.RoundToInt(Player.transform.position.z);
+        uiDistance.text = distance.ToString() + " m";
+
+
+        //se pressionar esc pausa
         if (Input.GetKeyUp(KeyCode.Escape))
         {
             if (GameIsPaused == true)
